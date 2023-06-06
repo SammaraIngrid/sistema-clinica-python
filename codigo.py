8489b1363993fcf8f939ae8991322d4fddde0f30
SammaraIngrid <sammaracavalcante2@gmail.com>
2023-06-06 17:47:56 -0300

    Refatoração de código

--- codigo.py
+++ codigo.py
@@ -9,74 +9,47 @@ from functions.consultas_db import marcar_consultas, listar_consultas, confirmar
 conn = sqlite3.connect('clinica') 
 c = conn.cursor()
 
-
-# Declarando variáveis:
-opcao = ""
-nome = ""
-contador = ""
-data = ""
-consultas = []
-posicao_consulta = 0
-confirmacao_consulta =  [["", "", ""] for _ in range(5)]
-
 # Determinando as funções: 
 
 def mostrarMenu():
-    print("\n1- Marcar consulta")
-    print("1- Marcar consulta")
-    print("2- Listar consulta de um dia")
-    print("3- confirmar consulta")
-    print("4- Sair")
-    opcao = input("Digite a opção desejada: ")
+    print("\n1- Cadastrar usuário")
+    print("2- Listar usuários")
+    print("3- Marcar consulta")
+    print("4- Listar consultas")
+    print("5- Confirmar consulta")
+    print("6- Buscar consultas")
+    print("7- Deletar consultas")
+    print("8- Sair")
+    opcao = input("\nDigite a opção desejada: ")
     
     return int(opcao)
-
-
-def popularVetorConsulta():
-
-    global posicao_consulta
-    consultas.append([nome, data, confirmacao_consulta])
-
-    posicao_consulta +=1
-
     
+
+def cadastrarUsuario():
+    nome = input("Digite o novo usuário: ")
+    senha = input("Digite a nova senha: ")
+    cadastrar_usuario(conn, nome, senha)
+
+def listarUsuarios():
+    usuarios = listar_usuarios(conn)
+    print(usuarios)
+
 def marcarConsulta():
-    global nome, data
-
-    print("\n-------------------------------\n")
-
     nome = input("Digite seu nome: ")
     data = input("Digite a data da consulta (DD/MM/AAAA): ")
-    popularVetorConsulta()
-    
-    print("CONSULTA MARCADA COM SUCESSO!")
+    marcar_consultas(conn, nome,data)
 
-    print("\n-------------------------------\n")
+def listarConsultas():
+    consultas = listar_consultas(conn)
+    print(consultas)
+
+def deletarConsultas():
+    id = int(input("Qual o id da consulta que deseja deletar? "))
+    deletar_consultas(conn, id)
 
 def buscarConsultas():
-
-    global posicao_consulta
-
-    print("\n-------------------------------\n")
-
-    print(f"Há {posicao_consulta} consulta(s) cadastrada(s) no sistema\n")
-    data = input("Digite a data desejada para buscar (DD/MM/AAAA): ")
-
-    print("\n-------------------------------\n")
-    print(f"Consultas na data: {data}")
-
-    encontrou_consultas = False
-
-    for contador in range(posicao_consulta):
-        if consultas[contador][1] == data:
-            print(f"Nome do paciente: {consultas[contador][0]}")
-
-            encontrou_consultas = True
-
-    if not encontrou_consultas:
-        print("NÃO FOI ENCONTRADA CONSULTAS NA DATA MENCIONADA")
-
-    print("\n-------------------------------\n")
+        nome = input("Digite nome para buscar: ")
+        buscar_consultas(conn, nome)
 
 def confirmarConsulta():
     nome = input("Digite o nome do paciente: ")
@@ -86,34 +59,27 @@ def confirmarConsulta():
 opcao = 0 
 
 print("\n-------------------------------\n")
-print("\nBEM-VINDO(A) AO SISTEMA")
 print("\n-------------------------------\n")
+print("\nBEM-VINDO(A) AO SISTEMA DA CLÍNICA")
+print("\nEscolha uma das opções abaixo: \n")
 
 while(opcao != 8):
     opcao = mostrarMenu()
 
     if opcao == 1:
-        nome = input("Digite o novo usuário: ")
-        senha = input("Digite a nova senha: ")
-        cadastrar_usuario(conn, nome, senha)
+        cadastrarUsuario()
     elif opcao == 2:
-        usuarios = listar_usuarios(conn)
-        print(usuarios)
+        listarUsuarios()
     elif opcao == 3:
-        nome = input("Digite seu nome: ")
-        data = input("Digite a data da consulta (DD/MM/AAAA): ")
-        marcar_consultas(conn, nome,data)
+        marcarConsulta()
     elif opcao == 4:
-        consultas = listar_consultas(conn)
-        print(consultas)
+        listarConsultas()
     elif opcao == 5:
          confirmarConsulta()
     elif opcao == 6:
-         id = int(input("Qual o id da consulta que deseja deletar? "))
-         deletar_consultas(conn, id)
+        buscarConsultas()
     elif opcao == 7:
-        nome = input("Digite nome para buscar: ")
-        buscar_consultas(conn, nome)
+        deletarConsultas()
     elif opcao == 8:
         break
     else: 
